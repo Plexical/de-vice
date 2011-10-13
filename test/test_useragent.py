bfba73e5f9fb13fa8c86758ff230ed3c1e2dd843
Jacob Oscarson <jacob@plexical.com>
2011-10-13 11:33:06 +0200

    Slightly more elegent code by proper use of py.test funcargs.

--- test/test_useragent.py
+++ test/test_useragent.py
@@ -5,16 +5,21 @@ from device import useragent
 from .user_agents import user_agents
 
 def pytest_generate_tests(metafunc):
+    args = {}
+
+    if 'analyzer' in metafunc.funcargnames:
+        args['analyzer'] = useragent.Analyzer()
 
     if "agent" in metafunc.funcargnames:
         for agent in user_agents:
-            metafunc.addcall(funcargs={'agent': agent})
+            metafunc.addcall(funcargs=dict(agent=agent, **args))
+    else:
+        metafunc.addcall(funcargs=args)
 
 def test_create_simplest():
     assert useragent.Analyzer()
 
-def test_calculate_poc():
-    analyzer = useragent.Analyzer()
+def test_calculate_poc(analyzer):
     info = analyzer.analyze('Dummy')
     assert info['type'] == 'unknown'
     info['digest'] in analyzer.cache
@@ -23,17 +28,17 @@ def test_uaencode():
     assert (useragent.uaencode("Opera/5.0 (SunOS 5.8 sun4u; U) [en]") ==
             "Opera/5.0_(SunOS_5.8_sun4u*_U)_[en]")
 
-def test_calculate_unknown_queries():
-    analyzer = useragent.Analyzer() # XXX Lame, learn to use funcargs instead
+def test_calculate_unknown_queries(analyzer):
     with patch.object(analyzer, 'query') as mocked_q:
         mocked_q.return_value = {'complete': True}
         analyzer.analyze('Dummy')
     assert mocked_q.called
 
-def test_analysis(agent):
-    analyzer = useragent.Analyzer() # XXX Lame, learn to use funcargs instead
-    agent, attrs = agent # XXX Also lame, same reason
-    info = analyzer.analyze(agent)
+def test_analysis(analyzer, agent):
+    agent, attrs = agent # XXX would be more elegant to separate agent, attrs
+    with patch.object(analyzer, 'query') as mocked_q:
+        mocked_q.return_value = {'complete': True}
+        info = analyzer.analyze(agent)
     assert info['complete']
     for attr, expected in attrs.iteritems():
         assert info[attr] == expected
